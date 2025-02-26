62837f51dc5f43c9efb60257d4bd89338f11a562
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2025-02-26 22:42:54 +0100

    add core, remove sqfc files

---
+++ addons/core/config.cpp
@@ -0,0 +1,20 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = QUOTE(COMPONENT);
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cba_main",
+            "hal_main"
+        };
+        author = "MiszczuZPolski";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgFactionClasses.hpp"
+#include "CfgVehicles.hpp"
---
+++ addons/core/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT core
+#define COMPONENT_BEAUTIFIED HAL - Core
+#include "\z\hal\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_HAL
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_HAL
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_HAL
+#endif
+
+#include "\z\hal\addons\main\script_macros.hpp"
